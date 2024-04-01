cda59cc6bc8e3c402ba7806492d4da403be59911
Niyazi Güven <121157684+Niygvn@users.noreply.github.com>
2024-04-01 18:31:47 +0300

    simple refoctor

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -1,27 +1,25 @@
 <template>
   <div class="container">
-    <div>
-      <input
-        v-model="filter"
-        placeholder="search:tt group:continent/language/currency"
-        @input="applyFilter"
-      />
-      <div v-if="isLoading">Loading...</div>
-      <div v-if="error">{{ error }}</div>
-      <div v-else>
-        <div v-for="(group, groupName) in groupedCountries" :key="groupName">
-          <h3>{{ groupName }}</h3>
-          <ul>
-            <li
-              v-for="country in group"
-              :key="country.code"
-              @click="toggleSelection(country)"
-              :style="{ backgroundColor: getBackgroundColor(country) }"
-            >
-              {{ country.name }}
-            </li>
-          </ul>
-        </div>
+    <input
+      v-model="filter"
+      placeholder="search:tt group:continent/language/currency"
+      @input="applyFilter"
+    />
+    <div v-if="isLoading">Loading...</div>
+    <div v-if="error">{{ error }}</div>
+    <div v-else>
+      <div v-for="(group, groupName) in groupedCountries" :key="groupName">
+        <h3>{{ groupName }}</h3>
+        <ul>
+          <li
+            v-for="country in group"
+            :key="country.code"
+            @click="toggleSelection(country)"
+            :style="{ backgroundColor: getBackgroundColor(country) }"
+          >
+            {{ country.name }}
+          </li>
+        </ul>
       </div>
     </div>
   </div>
